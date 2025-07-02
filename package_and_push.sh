8d26485525ef747eb920a77797a7d816646f95f7
Michael Hruby <galactus2121@gmail.com>
2025-07-02 15:58:48 +0200

    Remove: Getting rid of the broken sh script

--- package_and_push.sh
+++
@@ -1,3 +0,0 @@
-docker buildx build --platform linux/amd64,linux/arm64 \                                                                                                                                               ─╯
-  -t galactuscz/main-page:latest \
-  --push .
