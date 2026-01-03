08361a9dd6f5df29ecb5f7133b12ffbc919523c9
Michael Hrubý <galactus2121@gmail.com>
2026-01-03 14:37:47 +0100

    Fixing resume links

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -9,7 +9,7 @@
       <li>E-mail: <a href="mailto:michael-hruby@seznam.cz">michael-hruby@seznam.cz</a></li>
       <li>LinkedIn: <a href="https://www.linkedin.com/in/michael-hrub%C3%BD-b3a0542a8/" target="_blank">Michael Hrubý</a></li>
       <li>Github: <a href="https://github.com/GalactusCZ" target="_blank">GalactusCZ</a></li>
-      <li>Resume: <a href="/public/michael-hruby-english.pdf" target="_blank">English</a>, <a href="/public/michael-hruby-czech.pdf" target="_blank">Czech</a></li>
+      <li>Resume: <a href="/michael-hruby-english.pdf" target="_blank">English</a>, <a href="/michael-hruby-czech.pdf" target="_blank">Czech</a></li>
     </ul>
   </div>
 </template>
